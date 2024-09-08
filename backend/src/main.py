5260c5ea3ae0594cfab9ab4cf2a115e4ab24cb17
Winston Lin <winlinworks@gmail.com>
2024-09-08 06:17:31 -0500

    corrected logging

--- backend/src/main.py
+++ backend/src/main.py
@@ -21,7 +21,7 @@ logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler(sys.stdout)
 log_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 stream_handler.setFormatter(log_formatter)
-logger.addHandler
+logger.addHandler(stream_handler)
 
 
 @asynccontextmanager
