1c5ab5978d08d71354fee4277931423966264ef0
Chao Sun <sunchao8866@gmail.com>
2024-12-27 09:05:18 -0600

    fetch data from backend API

--- frontend/src/components/navbar/UserIcon.tsx
+++ frontend/src/components/navbar/UserIcon.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { LuUser2 } from "react-icons/lu";
+import { LuUser } from "react-icons/lu";
 
 function UserIcon() {
   return (
-    <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white">
+    <LuUser className="w-6 h-6 bg-primary rounded-full text-white">
       UserIcon
-    </LuUser2>
+    </LuUser>
   );
 }
 
