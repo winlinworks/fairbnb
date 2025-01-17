df4a25565669b8f33935236e64655849f0166fe1
Chao Sun <sunchao8866@gmail.com>
2025-01-17 14:39:17 -0600

    Cleanup clerk

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "./providers";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>
@@ -27,6 +26,6 @@ export default function RootLayout({
           </Providers>
         </body>
       </html>
-    </ClerkProvider>
+    </>
   );
 }
--- frontend/src/components/navbar/SignOutLink.tsx
+++ frontend/src/components/navbar/SignOutLink.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useToast } from "@/hooks/use-toast";
-import { SignOutButton } from "@clerk/nextjs";
 
 import React from "react";
 
@@ -10,11 +9,11 @@ function SignOutLink() {
     toast({ description: "You have been signed out." });
   };
   return (
-    <SignOutButton redirectUrl="/">
+    <>
       <button className="w-full text-left " onClick={handleLogout}>
         Logout
       </button>
-    </SignOutButton>
+    </>
   );
 }
 
