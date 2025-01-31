06e09354928669ce6d94a85e59d757467c7520d0
Winston Lin <winlinworks@gmail.com>
2025-01-31 16:02:54 -0600

    Added integration tests and implemented User CRUD endpoints using Django ORM; added pytest-django dependency and updated pytest config; commented legacy Listings code and added to-dos to update w/ Django ORM

--- backend/src/fairbnb/settings.py
+++ backend/src/fairbnb/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print("Loading settings...")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -75,11 +78,10 @@ WSGI_APPLICATION = "fairbnb.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# Load environment variables from .env file
+load_dotenv()
+
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,27 +2,12 @@ import logging
 import sys
 from contextlib import asynccontextmanager
 
-from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
+# import models
+from django.contrib.auth.models import User
+from fastapi import FastAPI, HTTPException
 
-from src.crud import (
-    create_listing,
-    create_user,
-    delete_listing,
-    delete_user,
-    read_listing,
-    read_listings,
-    read_user,
-    read_user_by_email,
-    read_users,
-    update_listing,
-    update_user,
-)
-from src.db import get_db
-from src.schemas import ListingCreate, ListingRead, UserCreate, UserRead
-
-load_dotenv()
+from src.schemas import UserCreate, UserRead
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -43,94 +28,114 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/users", response_model=UserRead)
-def post_user(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = read_user_by_email(db, user.email)
+def post_user(user: UserCreate):
+    # db_user = read_user_by_email(db, user.email)
 
+    # If user already exists, raise an error
+    db_user = User.objects.filter(email=user.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    return create_user(db, user)
+
+    # Else, create the user
+    return User.objects.create_user(
+        username=user.username, email=user.email, password=user.password
+    )
 
 
-@app.get("/users", response_model=list[UserRead])
-def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return read_users(db, skip, limit)
+# TODO(winlinworks): Update to use Django ORM
+# @app.get("/users", response_model=list[UserRead])
+# def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     return read_users(db, skip, limit)
 
 
 @app.get("/users/{user_id}", response_model=UserRead)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = read_user(db, user_id)
+def get_user(user_id: int):
+    db_user = User.objects.filter(id=user_id).first()
     if db_user is None:
         raise HTTPException(status_code=404, detail=f"User ID {user_id} not found")
     return db_user
 
 
+# TODO(winlinworks): Update to use Django ORM
 @app.put("/users/{user_id}", response_model=UserRead)
-def put_user(user_id: int, user: UserRead, db: Session = Depends(get_db)):
-    db_user = read_user(db, user_id)
+def put_user(user_id: int, user: UserCreate):
+    # Get user based on user_id
+    db_user = User.objects.filter(id=user_id).first()
+
     if db_user is None:
         raise HTTPException(status_code=404, detail=f"User ID {user_id} not found")
 
-    return update_user(db, user_id, user)
+    # Update user fields and save user
+    db_user.username = user.username
+    db_user.email = user.email
+    db_user.password = user.password
+    db_user.save()
+
+    return db_user
 
 
+# TODO(winlinworks): Update to use Django ORM
 @app.delete("/users/{user_id}")
-def remove_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = read_user(db, user_id)
+def remove_user(user_id: int):
+    # Get user based on user_id
+    db_user = User.objects.filter(id=user_id).first()
+
     if db_user is None:
         raise HTTPException(status_code=404, detail=f"User ID {user_id} not found")
 
-    return delete_user(db, user_id)
+    db_user.delete()
 
 
-@app.post("/users/{user_id}/listings", response_model=ListingRead)
-def post_listing(user_id: int, listing: ListingCreate, db: Session = Depends(get_db)):
-    db_user = read_user(db, user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail=f"User ID {user_id} not found")
+# TODO(winlinworks): Update to use Django ORM
+# @app.post("/users/{user_id}/listings", response_model=ListingRead)
+# def post_listing(user_id: int, listing: ListingCreate, db: Session = Depends(get_db)):
+#     db_user = read_user(db, user_id)
+#     if db_user is None:
+#         raise HTTPException(status_code=404, detail=f"User ID {user_id} not found")
 
-    return create_listing(db, listing, user_id)
+#     return create_listing(db, listing, user_id)
 
+# TODO(winlinworks): Update to use Django ORM
+# @app.get("/listings", response_model=list[ListingRead])
+# def get_listings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     return read_listings(db, skip, limit)
 
-@app.get("/listings", response_model=list[ListingRead])
-def get_listings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return read_listings(db, skip, limit)
+# TODO(winlinworks): Update to use Django ORM
+# @app.get("/listings/{listing_id}", response_model=ListingRead)
+# def get_listing(listing_id: int, db: Session = Depends(get_db)):
+#     db_listing = read_listing(db, listing_id)
+#     if db_listing is None:
+#         raise HTTPException(
+#             status_code=404, detail=f"Listing ID {listing_id} not found"
+#         )
+#     return db_listing
 
+# TODO(winlinworks): Update to use Django ORM
+# @app.put("/listings/{listing_id}", response_model=ListingRead)
+# def put_listing(listing_id: int, listing: ListingRead, db: Session = Depends(get_db)):
+#     # Verify if user exists
+#     db_user = read_user(db, listing.owner_id)
+#     if db_user is None:
+#         raise HTTPException(
+#             status_code=404, detail=f"User ID {listing.owner_id} not found"
+#         )
 
-@app.get("/listings/{listing_id}", response_model=ListingRead)
-def get_listing(listing_id: int, db: Session = Depends(get_db)):
-    db_listing = read_listing(db, listing_id)
-    if db_listing is None:
-        raise HTTPException(
-            status_code=404, detail=f"Listing ID {listing_id} not found"
-        )
-    return db_listing
+#     # Verify if listing exists
+#     db_listing = read_listing(db, listing_id)
+#     if db_listing is None:
+#         raise HTTPException(
+#             status_code=404, detail=f"Listing ID {listing_id} not found"
+#         )
 
+#     return update_listing(db, listing_id, listing)
 
-@app.put("/listings/{listing_id}", response_model=ListingRead)
-def put_listing(listing_id: int, listing: ListingRead, db: Session = Depends(get_db)):
-    # Verify if user exists
-    db_user = read_user(db, listing.owner_id)
-    if db_user is None:
-        raise HTTPException(
-            status_code=404, detail=f"User ID {listing.owner_id} not found"
-        )
+# TODO(winlinworks): Update to use Django ORM
+# @app.delete("/listings/{listing_id}")
+# def remove_listing(listing_id: int, db: Session = Depends(get_db)):
+#     db_listing = read_listing(db, listing_id)
+#     if db_listing is None:
+#         raise HTTPException(
+#             status_code=404, detail=f"Listing ID {listing_id} not found"
+#         )
 
-    # Verify if listing exists
-    db_listing = read_listing(db, listing_id)
-    if db_listing is None:
-        raise HTTPException(
-            status_code=404, detail=f"Listing ID {listing_id} not found"
-        )
-
-    return update_listing(db, listing_id, listing)
-
-
-@app.delete("/listings/{listing_id}")
-def remove_listing(listing_id: int, db: Session = Depends(get_db)):
-    db_listing = read_listing(db, listing_id)
-    if db_listing is None:
-        raise HTTPException(
-            status_code=404, detail=f"Listing ID {listing_id} not found"
-        )
-
-    return delete_listing(db, listing_id)
+#     return delete_listing(db, listing_id)
