3b196570530eb72c7e2070faaebf8156c90879ba
Winston Lin <winlinworks@gmail.com>
2025-02-16 18:29:22 -0600

    added docstrings and renamed kwargs for DBClient; added PropertyDBClient

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,8 +1,4 @@
-from src.fairbnb.models import User
-
-# TODO(winlinworks): Create a Django client class to handle database operations
-
-# User CRUD ops
+from src.fairbnb.models import Property, User
 
 
 class DBClient:
@@ -13,26 +9,67 @@ class DBClient:
     def __init__(self, model):
         self.model = model
 
-    def create(self, **kwargs):
-        obj = self.model.objects.create(**kwargs)
+    def create(self, **fields):
+        """
+        Create an object in the database.
+
+        Args:
+            **fields: The fields to set on the object.
+
+        Returns:
+            The ID of the created object.
+        """
+        obj = self.model.objects.create(**fields)
         return obj.id
 
-    def read(self, **kwargs):
-        return self.model.objects.filter(**kwargs).first()
+    def read(self, **filters):
+        """
+        Read an object from the database.
 
-    def update(self, id: int, **kwargs):
+        Args:
+            **filters: The fields to filter by.
+
+        Returns:
+            The object if found, None otherwise.
+        """
+        return self.model.objects.filter(**filters).first()
+
+    def update(self, id: int, **fields):
+        """
+        Update an object in the database.
+
+        Args:
+            id (int): The ID of the object to update.
+            **fields: The fields to update and their new values.
+
+        Returns:
+            The updated object.
+        """
         obj = self.read(id=id)
-        for key, value in kwargs.items():
+        for key, value in fields.items():
             setattr(obj, key, value)
         obj.save()
         return obj
 
     def delete(self, id: int):
+        """
+        Delete an object from the database.
+
+        Args:
+            id (int): The ID of the object to delete.
+        """
         obj = self.read(id=id)
         obj.delete()
 
-    def check_record_exists(self, field: str, value: str) -> bool:
-        return self.read(**{field: value}) is not None
+    def check_record_exists(self, **filters) -> bool:
+        """
+        Check if a record exists in the database.
+
+        Args:
+            field (str): The field to check.
+            value (str): The value to check.
+        """
+        return self.read(**filters) is not None
 
 
 # User DB client
@@ -45,42 +82,11 @@ class UserDBClient(DBClient):
         super().__init__(User)
 
 
-# # Property CRUD ops
+# Property DB client
+class PropertyDBClient(DBClient):
+    """
+    Property database client to handle CRUD operations.
+    """
 
-
-# def create_listing(listing: PropertyCreate, user_id: int) -> Property:
-#     db_listing = Property(**listing.model_dump(), owner_id=user_id)
-#     db.add(db_listing)
-#     db.commit()
-#     db.refresh(db_listing)
-#     return db_listing
-
-
-# def read_listing(listing_id: int) -> Property:
-#     return db.query(Property).filter(Property.id == listing_id).first()
-
-
-# def read_listings(skip: int = 0, limit: int = 100) -> list[Property]:
-#     return db.query(Property).offset(skip).limit(limit).all()
-
-
-# def update_listing(
-#     listing_id: int, new_listing_data: PropertyRead
-# ) -> Property:
-#     db_listing = read_listing(db, listing_id)
-
-#     # Update listing fields
-#     update_record(db_listing, new_listing_data)
-
-#     # Save changes
-#     db.add(db_listing)
-#     db.commit()
-#     db.refresh(db_listing)
-#     return db_listing
-
-
-# def delete_listing(listing_id: int):
-#     db_listing = read_listing(db, listing_id)
-#     db.delete(db_listing)
-#     db.commit()
-#     return {"message": "Property deleted", "listing_id": listing_id}
+    def __init__(self):
+        super().__init__(Property)
