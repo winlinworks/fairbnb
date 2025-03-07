f10e5d941bb1d494cff602578802bdcdf2bedaa6
Chao Sun <sunchao8866@gmail.com>
2025-03-07 10:57:12 -0600

    Fixing errors from empty property list

--- frontend/src/components/home/PropertiesList.tsx
+++ frontend/src/components/home/PropertiesList.tsx
@@ -4,9 +4,13 @@ import type { PropertyCardProps } from "@/utils/types";
 function PropertiesList({ properties }: { properties: PropertyCardProps[] }) {
   return (
     <section className="mt-4 gap-8 grid sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4">
-      {properties.map((property) => {
-        return <PropertyCard key={property.id} property={property} />;
-      })}
+      {Array.isArray(properties) && properties.length > 0 ? (
+        properties.map((property) => {
+          return <PropertyCard key={property.id} property={property} />;
+        })
+      ) : (
+        <p>No properties available</p>
+      )}
     </section>
   );
 }
